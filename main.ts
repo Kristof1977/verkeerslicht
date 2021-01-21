48aadd0191bc4b00994e4bb4e15b0f707a90627c
Kristof1977 <77786130+Kristof1977@users.noreply.github.com>
2021-01-21 13:40:48 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
     basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
     basic.pause(2000)
     pins.digitalWritePin(DigitalPin.P0, 0)
@@ -8,7 +9,5 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P2, 1)
     basic.pause(5000)
     pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(2000)
     pins.digitalWritePin(DigitalPin.P1, 0)
 })
